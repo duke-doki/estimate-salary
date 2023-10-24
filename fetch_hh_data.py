951ec45aefffc014a11fab4d096af030a6691440
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-10-24 20:54:19 +0300

    make code readable

--- fetch_hh_data.py
+++ fetch_hh_data.py
@@ -1,26 +1,7 @@
-import requests
 import pprint
 from time import sleep
 
-url = 'https://api.hh.ru/vacancies'
-
-languages = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go']
-
-# vacancies_amount = {}
-# for language in languages:
-#     params = {'area': '1', 'period': '30', 'text': f'программист {language}'}
-#     response = requests.get(url, params=params)
-#     response.raise_for_status()
-#     vacancies_amount[f'{language}'] = response.json()['found']
-#
-# pprint.pprint(vacancies_amount)
-
-
-# params = {'area': '1', 'period': '30', 'text': 'программист python'}
-# response = requests.get(url, params=params)
-# response.raise_for_status()
-#
-# vacancies = response.json()
+import requests
 
 
 def predict_rub_salary(vacancy):
@@ -35,33 +16,47 @@ def predict_rub_salary(vacancy):
         return None
 
 
-average_salary = {}
+if __name__ == '__main__':
+    url = 'https://api.hh.ru/vacancies'
+    languages = [
+        'JavaScript', 'Java', 'Python',
+        'Ruby', 'PHP', 'C++',
+        'C#', 'C', 'Go'
+    ]
 
-for language in languages:
-    page = 0
-    pages_number = 1
-    all_vacancies = []
-    while page < pages_number:
-        params = {'area': '1', 'period': '30', 'text': f'программист {language}', 'page': page}
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        vacancies = response.json()
-        average_salary[f'{language}'] = {'vacancies_found': vacancies['found']}
-        all_vacancies.extend(vacancies['items'])
-        pages_number = vacancies['pages']
-        page += 1
+    average_salary = {}
+    for language in languages:
+        page = 0
+        pages_number = 1
+        all_vacancies = []
+        while page < pages_number:
+            params = {
+                'area': '1', 'period': '30',
+                'text': f'программист {language}', 'page': page
+            }
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            vacancies = response.json()
+            average_salary[f'{language}'] = {
+                'vacancies_found': vacancies['found']
+            }
+            all_vacancies.extend(vacancies['items'])
+            pages_number = vacancies['pages']
+            page += 1
 
+        vacancies_processed = []
+        for vacancy in all_vacancies:
+            if vacancy['salary']:
+                vacancy_salary = predict_rub_salary(vacancy)
+                if vacancy_salary:
+                    vacancies_processed.append(vacancy_salary)
 
-    vacancies_processed = []
-    for vacancy in all_vacancies:
-        if vacancy['salary']:
-            vacancy_salary = predict_rub_salary(vacancy)
-            if vacancy_salary:
-                vacancies_processed.append(vacancy_salary)
+        average_salary[f'{language}']['vacancies_processed'] = (
+            len(vacancies_processed)
+        )
+        average_salary[f'{language}']['average_salary'] = (
+            int(sum(vacancies_processed) / len(vacancies_processed))
+        )
+        sleep(30)
 
-    average_salary[f'{language}']['vacancies_processed'] = len(vacancies_processed)
-    average_salary[f'{language}']['average_salary'] = int(sum(vacancies_processed) / len(vacancies_processed))
-    print(f'{language} finished, wait 30 secs')
-    sleep(30)
-
-pprint.pprint(average_salary)
+    pprint.pprint(average_salary)
--- fetch_sj_data.py
+++ fetch_sj_data.py
@@ -1,8 +1,8 @@
-import requests
 import pprint
-from time import sleep
 
-url = 'https://api.superjob.ru/2.0/vacancies/'
+import requests
+from environs import Env
+
 
 def predict_rub_salary_for_superJob(vacancy):
     if vacancy['currency'] == 'rub':
@@ -17,41 +17,52 @@ def predict_rub_salary_for_superJob(vacancy):
     else:
         return None
 
-#
-# for profession in vacancies['objects']:
-#     print(profession['profession'], profession['town']['title'], predict_rub_salary_for_superJob(profession), sep=', ')
 
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    sj_token = env.str('SJ_TOKEN')
+    url = 'https://api.superjob.ru/2.0/vacancies/'
+    languages = [
+        'JavaScript', 'Java', 'Python',
+        'Ruby', 'PHP', 'C++',
+        'C#', 'C', 'Go'
+        ]
 
-languages = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#', 'C', 'Go']
+    average_salary = {}
+    for language in languages:
+        page = 0
+        pages_number = 1
+        all_vacancies = []
+        while page < pages_number:
+            headers = {'X-Api-App-Id': sj_token}
+            params = {
+                'town': 4, 'catalogues': 48,
+                'keyword': f'{language}', 'page': page
+            }
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
+            vacancies = response.json()
+            average_salary[f'{language}'] = {
+                'vacancies_found': vacancies['total']
+            }
+            all_vacancies.extend(vacancies['objects'])
+            pages_number = round(vacancies['total'] / 20)
+            page += 1
 
-average_salary = {}
+        vacancies_processed = []
+        for vacancy in all_vacancies:
+            vacancy_salary = predict_rub_salary_for_superJob(vacancy)
+            if vacancy_salary:
+                vacancies_processed.append(vacancy_salary)
 
-for language in languages:
-    page = 0
-    pages_number = 1
-    all_vacancies = []
-    while page < pages_number:
-        headers = {'X-Api-App-Id': 'v3.r.137911002.85c67749ae965fae86ac9430b987ce8e01d8e189.1ab43e2ac3c475c22095628cf25570809689b468'}
-        params = {'town': 4, 'catalogues': 48, 'keyword': f'{language}', 'page': page}
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        vacancies = response.json()
-        average_salary[f'{language}'] = {'vacancies_found': vacancies['total']}
-        all_vacancies.extend(vacancies['objects'])
-        pages_number = round(vacancies['total'] / 20)
-        page += 1
+        average_salary[f'{language}']['vacancies_processed'] = (
+            len(vacancies_processed)
+        )
 
+        if len(vacancies_processed):
+            average_salary[f'{language}']['average_salary'] = (
+                int(sum(vacancies_processed) / len(vacancies_processed))
+            )
 
-    vacancies_processed = []
-    for vacancy in all_vacancies:
-        vacancy_salary = predict_rub_salary_for_superJob(vacancy)
-        if vacancy_salary:
-            vacancies_processed.append(vacancy_salary)
-
-    average_salary[f'{language}']['vacancies_processed'] = len(vacancies_processed)
-    if len(vacancies_processed):
-        average_salary[f'{language}']['average_salary'] = int(sum(vacancies_processed) / len(vacancies_processed))
-    # print(f'{language} finished, wait 30 secs')
-    # sleep(30)
-
-pprint.pprint(average_salary)
+    pprint.pprint(average_salary)
