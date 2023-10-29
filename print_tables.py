8b8f6954a3b9774fe985c1bd7c39585133061027
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-10-29 12:05:27 +0300

    fix code

--- print_tables.py
+++ print_tables.py
@@ -1,7 +1,15 @@
+from environs import Env
+
+from fetch_data_helper import make_table
 from fetch_headhunter import fetch_headhunter
 from fetch_superjob import fetch_superjob
 
 
 if __name__ == '__main__':
-    fetch_headhunter()
-    fetch_superjob()
+    env = Env()
+    env.read_env()
+    sj_key = env.str('SJ_KEY')
+    headhunter_title = 'HeadHunter Moscow'
+    print(make_table(fetch_headhunter(), headhunter_title))
+    superjob_title = 'SuperJob Moscow'
+    print(make_table(fetch_superjob(sj_key), superjob_title))
